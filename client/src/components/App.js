cd1fba48cb5276ad85007e50ebd83ca6fb56c399
Yoona jeong <yoonalove77@naver.com>
2022-09-07 18:07:22 +0900

    make landing page UI/UX

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,19 +7,30 @@ import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
 
 //admin page
-import UploadPage from './views/UploadPage/UploadPage';
+import AdminPage from './views/AdminPage/AdminPage';
+import UploadPage from './views/AdminPage/UploadPage/UploadPage'
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
 
 function App() {
+  if (LandingPage) {
+    return (
+      <Suspense fallback={(<div>Loading...</div>)}>
+          <Switch>
+            <Route exact path="/" component={Auth(LandingPage, null)} />
+          </Switch>
+    </Suspense>
+    )
+  }
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
       <NavBar />
       <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/admin" component={Auth(AdminPage, null)} />
           <Route exact path="/admin/upload" component={Auth(UploadPage, null)} />
         </Switch>
       </div>
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,37 @@
+import { Button } from 'antd'
 import React from 'react'
+import Logo from './img/Logo.png'
+import Poster from './img/Poster.png'
+// import '../../../index.css'
+import '../LandingPage/LandingPage.css'
 
 function LandingPage() {
+
     return (
-        <>
-            <div className="app">
-                <span style={{ fontSize: '2rem' }}>Main Landing Page</span>
+        <div style={{ background: '#282828', width: '100%', height: '100%' }}>
+            <div style={{ display: 'flex', width: '85%' }}>
+                <img src={Logo} style={{ width: '130px', height: '65px', margin: '52px 0px 0px 230px' }} />
+                <hr style={{ color: '#7F7F7F', width: '80%', height: '1px', background: '#7F7F7F', margin: '90px 0px 0px 32px', border: 0 }} />
             </div>
-        </>
+            <div style={{ width: '80%', height: '820px', background: '#343434', margin: '0px auto', marginTop: '28px', borderRadius: '50px'}}>
+                <p id='gradient'>1st</p>
+                <p id='main'>EXHIBITION,</p>
+                <p id='main'>SHOW YOUR RELAXATION</p>
+                <Button>Museum</Button>
+                <Button>About Us</Button>
+                <div>
+                    <p>From</p>
+                    <p>NOV 4th</p>
+                </div>
+                <div>
+                    <p>To</p>
+                    <p>NOV 13th</p>
+                </div>
+                <div style={{width: '600px', height: '600px'}}>
+                    <img src={Poster} />
+                </div>
+            </div>
+        </div>
     )
 }
 
