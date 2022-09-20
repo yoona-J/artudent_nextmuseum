04c02c8a4897e050a5c9864cf3f69eebbd949eec
Yoona jeong <yoonalove77@naver.com>
2022-09-20 17:01:42 +0900

    make navbar, footer, aboutPage UI use media-query

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from './img/Logo.png'
+import './Footer.css'
 
 function Footer() {
 
@@ -10,11 +11,11 @@ function Footer() {
             </>
         } else {
             return <>
-            <div style={{backgroundColor: '#3E3E3E', width: '100%', height: '200px', display: 'inline-block'}}>
+            <div className='footer_div' style={{backgroundColor: '#3E3E3E', width: '100%', height: '200px', display: 'inline-block'}}>
                 <a href='/'>
                     <img src={Logo} style={{ width: '130px', height: '65px', margin: '67px 0px 68px 110px'}}/>
                 </a>
-                <div style={{ float: 'right', margin: '46px 46px 46px 0px', color: '#fff'}}>
+                <div style={{float: 'right', margin: '46px 46px 46px 0px', color: '#fff', textAlign: 'right'}}>
                     <p>고객 문의</p>
                     <a href='/about' style={{color: '#fff'}}>
                         <p>About Artudent</p>
